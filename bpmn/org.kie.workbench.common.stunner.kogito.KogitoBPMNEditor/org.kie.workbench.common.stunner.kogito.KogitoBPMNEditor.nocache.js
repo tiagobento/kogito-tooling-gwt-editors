f9209786fb97e9821372c210ebe49052eca6bdb6
Tiago Bento <tiagobentofernandes@gmail.com>
2019-08-23 09:39:58 -0300

    BPMN fixed

--- bpmn/org.kie.workbench.common.stunner.kogito.KogitoBPMNEditor/org.kie.workbench.common.stunner.kogito.KogitoBPMNEditor.nocache.js
+++ bpmn/org.kie.workbench.common.stunner.kogito.KogitoBPMNEditor/org.kie.workbench.common.stunner.kogito.KogitoBPMNEditor.nocache.js
@@ -331,7 +331,7 @@ function org_kie_workbench_common_stunner_kogito_KogitoBPMNEditor(){
     }
     var strongName;
     try {
-      strongName = '2C121D8402A07BC0352A9E6CE8429801';
+      strongName = '879EA7AF0D51BCEFB67EC06EE318E8F3';
       var idx = strongName.indexOf(':');
       if (idx != -1) {
         softPermutationId = parseInt(strongName.substring(idx + 1), 10);
